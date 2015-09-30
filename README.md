49b86c3d5588cf8836ee0fd3d49823f738ac01e7
Cam Song <neosoyn@gmail.com>
2015-09-30 16:20:12 +0800

    open source

---
+++ README.md
@@ -0,0 +1,23 @@
+## Fetch Detector
+
+有些浏览器虽然支持 Fetch API 但不完整，如 Chrome 45 和 Firefox 33 版本 response
+中的[中文会乱码](https://lists.w3.org/Archives/Public/public-webapps-github/2015Aug/0218.html)。本库的作用是只在 Chrome >= 46，Firefox >= 39 时才开启原生
+Fetch，否则使用 XHR polyfill。
+
+## Install
+
+```
+npm install 'fetch-detector'
+```
+
+## Usage
+
+一定要与 fetch-ie8 一起使用， 并且放到 fetch-ie8 前引用。
+
+```js
+import 'fetch-detector'
+import 'fetch-ie8'
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,26 @@
+// 检测浏览器版本，并设置 window.__disableNativeFetch 来决定是否开启 fetch
+var userAgent = navigator.userAgent || "";
+var appVersion = navigator.appVersion || "";
+var vendor = navigator.vendor || "";
+
+var ua = (userAgent + ' ' + appVersion + ' ' + vendor).toLowerCase();
+
+var match = /(chrome)[ \/]([\w.]+)/.exec(ua)
+      || /(webkit)[ \/]([\w.]+)/.exec(ua)
+      || /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua)
+      || /(msie) ([\w.]+)/.exec(ua)
+      || /(trident)(?:.*? rv:([\w.]+)|)/.exec(ua)
+      || ua.indexOf("compatible") < 0
+      && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) || [];
+
+var engine = match[0];
+var mainVersion = match[2].split('.')[0];
+
+// chrome 内核版本大于 46， firefox 版本大于39 才开启 fetch
+if (engine.indexOf('chrome') === 0 && mainVersion >= 46) {
+  window.__disableNativeFetch = false;
+} else if (engine.indexOf('mozilla') === 0 && mainVersion >= 39) {
+  window.__disableNativeFetch = false;
+} else {
+  window.__disableNativeFetch = true;
+}
